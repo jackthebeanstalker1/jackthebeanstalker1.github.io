31aa0f2707ecd670f96c87915b191c631ccdb009
jackthebeanstalker1 <jackthebeanstalker1@gmail.com>
2021-04-09 19:57:47 -0400

    fix code to use my repo

--- jackthebeanstalker1_githubio/deploy.sh
+++ jackthebeanstalker1_githubio/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/hello-world.git master:gh-pages
+git push -f git@github.com:jackthebeanstalker1/jackthebeanstalker1.github.io.git master:gh-pages
 cd -
